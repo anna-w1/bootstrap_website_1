dde70907359c7dbb67111118e6058fe276a80a5e
anna-w1 <anna.evg.w@gmail.com>
2023-08-08 13:45:50 +0300

    mobile

---
+++ src/js/scritpt.js
@@ -0,0 +1,49 @@
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++){
+    
+//     for (let j = 0; j < i; j++){
+//         result += "*";
+//     }
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// let num = 5;
+
+// while (num <= 10) {
+//     console.log(num);
+
+//     num++;
+
+// }
+
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//   }
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+let num = 2;
+
+while (num <= 16) {
+
+    console.log(num);
+        num++;
+    if (num % 2 === 0);{
+        continue;
+
+    }
+
+
+}
